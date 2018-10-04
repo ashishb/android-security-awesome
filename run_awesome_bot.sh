8e13559496e3474bf01e37a747f33b7555f888f0
Ashish Bhatia <ashishb@ashishb.net>
2018-10-04 00:10:47 -0700

    More cleanup + more URLs whitelisted

--- run_awesome_bot.sh
+++ run_awesome_bot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-awesome_bot 
+awesome_bot \
   --allow-redirect \
   --allow-ssl \
-  --white-list 'opencollective.com','http://copperdroid.isg.rhul.ac.uk/copperdroid/','http://sanddroid.xjtu.edu.cn/','http://www.foresafe.com/scan','https://github.com/BaiduSecurityLabs/ZjDroid','https://github.com/yangbean9/ZjDroid','https://appanalysis.org/download.html','https://labs.mwrinfosecurity.com/tools/2012/03/16/mercury/','https://dexter.dexlabs.org/','http://www.mobiseclab.org/eacus.jsp','https://fireeye.ijinshan.com/','http://www.comdroid.org/','http://www.androidsandbox.net/','http://andrototal.org','http://www.mobile-app-insight.org','http://anubis.iseclab.org/','http://blog.avlyun.com/wp-content/uploads/2014/04/SmaliViewer.zip' \
+  --white-list 'opencollective.com','http://copperdroid.isg.rhul.ac.uk/copperdroid/','http://sanddroid.xjtu.edu.cn/','http://www.foresafe.com/scan','https://github.com/BaiduSecurityLabs/ZjDroid','https://github.com/yangbean9/ZjDroid','https://appanalysis.org/download.html','https://labs.mwrinfosecurity.com/tools/2012/03/16/mercury/','https://dexter.dexlabs.org/','http://www.mobiseclab.org/eacus.jsp','https://fireeye.ijinshan.com/','http://www.comdroid.org/','http://www.androidsandbox.net/','http://andrototal.org','http://www.mobile-app-insight.org','http://anubis.iseclab.org/','http://blog.avlyun.com/wp-content/uploads/2014/04/SmaliViewer.zip','habo.qq.com','www.fasteque.com' \
   --files README.md

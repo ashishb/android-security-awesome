a0a567127feb6b15747d9f784cb987d4914ed3f1
Ashish Bhatia <ashishb@ashishb.net>
2019-01-28 10:44:11 -0800

    Mark TraceDroid dead
    Also ignore appanalysis.org, it works but for some reason, Travis CI
    times out while trying to access it.
    
    ```
    Issues :-(
    > Links
      1. [L107]  http://appanalysis.org Connection reset by peer
    > Dupes
      None ✓
    The command "bash run_awesome_bot.sh" exited with 1.
    ```

--- run_awesome_bot.sh
+++ run_awesome_bot.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 set -euxo pipefail
 
 
-DEAD_URLS='opencollective.com','http://copperdroid.isg.rhul.ac.uk/copperdroid/','http://sanddroid.xjtu.edu.cn/','http://www.foresafe.com/scan','https://github.com/BaiduSecurityLabs/ZjDroid','https://github.com/yangbean9/ZjDroid','https://appanalysis.org/download.html','https://labs.mwrinfosecurity.com/tools/2012/03/16/mercury/','https://dexter.dexlabs.org/','http://www.mobiseclab.org/eacus.jsp','https://fireeye.ijinshan.com/','http://www.comdroid.org/','http://www.androidsandbox.net/','http://andrototal.org','http://www.mobile-app-insight.org','http://anubis.iseclab.org/','http://blog.avlyun.com/wp-content/uploads/2014/04/SmaliViewer.zip','habo.qq.com','www.fasteque.com','http://admire.necst.it/' \
+DEAD_URLS='opencollective.com','http://copperdroid.isg.rhul.ac.uk/copperdroid/','http://sanddroid.xjtu.edu.cn/','http://www.foresafe.com/scan','https://github.com/BaiduSecurityLabs/ZjDroid','https://github.com/yangbean9/ZjDroid','https://appanalysis.org/download.html','https://labs.mwrinfosecurity.com/tools/2012/03/16/mercury/','https://dexter.dexlabs.org/','http://www.mobiseclab.org/eacus.jsp','https://fireeye.ijinshan.com/','http://www.comdroid.org/','http://www.androidsandbox.net/','http://andrototal.org','http://www.mobile-app-insight.org','http://anubis.iseclab.org/','http://blog.avlyun.com/wp-content/uploads/2014/04/SmaliViewer.zip','habo.qq.com','www.fasteque.com','http://admire.necst.it/','tracedroid.few.vu.nl','appanalysis.org' \
 FLAKY_URLS='http://safe.ijiami.cn/'
 SRC_FILE=README.md
+# Run `gem install awesome_bot` to install awesome_bot
 awesome_bot \
   --allow-redirect \
   --allow-ssl \
